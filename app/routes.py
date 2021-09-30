33f8be3bce87d90b1934f568a6091cedecb36ac7
David Sanders <david.wm.sanders@gmail.com>
2021-09-30 04:19:42 +0100

    Save some data to the db

--- app/routes.py
+++ app/routes.py
@@ -113,21 +113,76 @@ def set_profile():
     player_elements = data_xml.findall("./player")
     # todo: check to make sure we have some data here
     # print(f"{players=}")
+    updated_players = []
     for player_elem in player_elements:
         playerdc = PlayerDc.from_element(player_elem)
-        print(f"{playerdc=}")
-        print(f"{playerdc.person=}")
-        print(f"{playerdc.profile=}")
-        # hash_, rid = player.attrib["hash"], player.attrib["rid"]
-        # # todo: check hash in db for realm and rid matches for hash
+
+        # check hash in db for realm and rid matches for hash
         try:
             player = Player.query.filter_by(hash=playerdc.hash_, realm_id=realm.id).one()
         except NoResultFound as e:
             # this player doesn't exist
             print(f"set profile error: player ({realm.id}, {playerdc.hash_}) not found, won't update, skipping...")
             continue
-        print(f"{player=}")
 
+        # print(f"{player=}")
+
+        # more validations:
+        # check rid
+        if playerdc.rid != player.rid:
+            print(f"set profile error: player ({realm.id}, {playerdc.hash_}) evil rid")
+            continue
+        # todo: check steam id
+        # issue: sid not set at get, sent first time in a set_profile, will be null/None
+        # need to check this and skip if None
+        # if playerdc.profile.sid != player.sid:
+        #     # print(f"set profile error: player ({realm.id}, {playerdc.hash_}) sid mismatch")
+        #     continue
+
+        # create a player mapping for bulk_insert_mappings?
+        # we need to specify hash and realm_id because they constitute the primary key
+        # we can omit rid because it should stay constant?
+        # and the rid check should already have identified a problem with this player set spec
+        # we omit username because it was already set by the get and should be immutable
+        # we must "update" the sid because it is not specified in the original get
+        # i.e. this could be the first set for a specific profile
+        i = playerdc.person.items
+        item0, item1, item2, item4, item5 = i[0], i[1], i[2], i[4], i[5]
+        s = playerdc.profile.stats
+        pm = dict(hash=playerdc.hash_, realm_id=realm.id,
+                  # basic profile stuff
+                  sid=playerdc.profile.sid, game_version=playerdc.profile.game_version,
+                  squad_tag=playerdc.profile.squad_tag, color=playerdc.profile.color,
+                  # basic person stuff
+                  max_authority_reached=playerdc.person.max_authority_reached,
+                  authority=playerdc.person.authority, job_points=playerdc.person.job_points,
+                  faction=playerdc.person.faction, name=playerdc.person.name, alive=playerdc.person.alive,
+                  soldier_group_id=playerdc.person.soldier_group_id,
+                  soldier_group_name=playerdc.person.soldier_group_name,
+                  squad_size_setting=playerdc.person.squad_size_setting,
+                  squad_config_index=playerdc.person.squad_config_index,
+                  blockx=None, blocky=None, order_moving=None, order_target=None, order_class=None,
+                  item0_index=item0.index, item0_amount=item0.amount, item0_key=item0.key,
+                  item1_index=item1.index, item1_amount=item1.amount, item1_key=item1.key,
+                  item2_index=item2.index, item2_amount=item2.amount, item2_key=item2.key,
+                  item4_index=item4.index, item4_amount=item4.amount, item4_key=item4.key,
+                  item5_index=item5.index, item5_amount=item5.amount, item5_key=item5.key,
+                  # profile stats
+                  kills=s.kills, deaths=s.deaths, time_played=s.time_played,
+                  player_kills=s.player_kills, teamkills=s.teamkills, longest_kill_streak=s.longest_kill_streak,
+                  targets_destroyed=s.targets_destroyed, vehicles_destroyed=s.vehicles_destroyed,
+                  soldiers_healed=s.soldiers_healed, times_got_healed=s.times_got_healed,
+                  distance_moved=s.distance_moved,
+                  shots_fired=s.shots_fired, throwables_thrown=s.throwables_thrown,
+                  rank_progression=s.rank_progression)
+
+        print(f"{pm=}")
+        updated_players.append(pm)
+
+    print(f"set profile: updating {len(updated_players)} players...")
+    db.session.bulk_update_mappings(Player, updated_players)
+    print(f"set profile: committing updates...")
+    db.session.commit()
 
     return ""
 
