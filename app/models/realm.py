eb49f3ab4ad2cfb0ced4c729122941bba13939a1
David Sanders <david.wm.sanders@gmail.com>
2021-10-18 05:54:32 +0100

    Add missing itemgroup_id column in realm

--- app/models/realm.py
+++ app/models/realm.py
@@ -5,4 +5,5 @@ class Realm(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), index=True, unique=True, nullable=False)
     digest = db.Column(db.String(64), nullable=False)
+    itemgroup_id = db.Column(db.Integer, db.ForeignKey("item_group.id"), nullable=False)
     accounts = db.relationship("BasicAccount", backref="realm", lazy="dynamic")
