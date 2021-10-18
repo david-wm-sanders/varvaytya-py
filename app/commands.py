f973ff50b19356f29e487c5793aaf7b36d0670b0
David Sanders <david.wm.sanders@gmail.com>
2021-10-18 05:58:12 +0100

    Add create-items flask command

--- app/commands.py
+++ app/commands.py
@@ -1,4 +1,7 @@
 import csv
+import pathlib
+
+import click
 
 from app import app, db
 from app.models import ItemGroup, ItemDef, Realm
@@ -6,3 +9,47 @@ from app.models import ItemGroup, ItemDef, Realm
 
 # todo: write flask command to create ItemGroup - loading item defs from csv created by index_items.py
 # todo: write flask command to create Realm - requiring name, digest, and itemgroup id
+
+
+@app.cli.command("create-items")
+@click.argument("name", required=True)
+@click.argument("items_csv_path", required=True)
+@click.argument("storage_width", type=int, default=10)
+def create_items(name, items_csv_path, storage_width):
+    print(f"Creating item group '{name}' [sw:{storage_width}] from '{items_csv_path}'...")
+    items_csv_p = pathlib.Path(items_csv_path)
+    if not items_csv_p.exists():
+        print(f"Error: '{items_csv_path}' doesn't exist!")
+        return
+
+    items = []
+    print(f"Reading '{items_csv_p}'...")
+    with items_csv_p.open("r", encoding="utf-8") as csv_f:
+        reader = csv.DictReader(csv_f)
+        for row in reader:
+            items.append((row["class"], row["index"], row["key"]))
+    print(f"Read {len(items)} items!")
+
+    print(f"Creating item group '{name}'...")
+    ig = ItemGroup(name=name, storage_width=storage_width)
+    db.session.add(ig)
+    db.session.commit()
+    print(f"Created item group '{name}' [{ig.id}]!")
+
+    print("Constructing item definitions...")
+    itemdefs = [{"itemgroup_id": ig.id, "id": x,
+                 "cls": item[0], "dex": item[1], "key": item[2]} for x, item
+                in enumerate(items)]
+    print("Bulk inserting item definitions...")
+    db.session.bulk_insert_mappings(ItemDef, itemdefs)
+    print("Committing...")
+    db.session.commit()
+    print("Success!")
+
+
+@app.cli.command("_delete_ig")
+@click.argument("itemgroup_id", type=int, required=True)
+def _delete_ig(itemgroup_id: int):
+    db.session.query(ItemGroup).filter_by(id=itemgroup_id).delete()
+    db.session.query(ItemDef).filter_by(itemgroup_id=itemgroup_id).delete()
+    db.session.commit()
