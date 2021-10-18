9205a97ee68197ff5347f94fb0e4634b6778b69b
David Sanders <david.wm.sanders@gmail.com>
2021-10-18 04:14:01 +0100

    Fix relationship in realm model

--- app/models/realm.py
+++ app/models/realm.py
@@ -5,4 +5,4 @@ class Realm(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), index=True, unique=True, nullable=False)
     digest = db.Column(db.String(64), nullable=False)
-    accounts = db.relationship("Account", backref="realm", lazy="dynamic")
+    accounts = db.relationship("BasicAccount", backref="realm", lazy="dynamic")
